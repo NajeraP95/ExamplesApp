f78608b3cc200aab74c6dd326ebcde08313c8cbf
Jorge Najera <jorgenajera095@outlook.com>
2020-01-26 19:48:58 -0600

    Intro Programation started :wink:
    Intro items and view pager adapter added

---
+++ app/src/main/java/com/najera/examplesapp/IntroScreenItem.java
@@ -0,0 +1,34 @@
+package com.najera.examplesapp;
+
+public class IntroScreenItem {
+
+    String title, description;
+    int imgScreen;
+
+
+
+    //Getter and setter for screen items
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImgScreen() {
+        return imgScreen;
+    }
+
+    public void setImgScreen(int imgScreen) {
+        this.imgScreen = imgScreen;
+    }
+}
---
+++ app/src/main/java/com/najera/examplesapp/IntroViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.najera.examplesapp;
+
+public class IntroViewPagerAdapter {
+}
